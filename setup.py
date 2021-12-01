eda0e86c60e1e253d2dcd636376d0a1674f6c66d
kriii <kriiivv@gmail.com>
2021-12-01 16:18:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,39 @@
 from setuptools import setup, find_packages
 
-requirements = [ ]
+requirements = [
+    "pycryptodome",
+    "coincurve",
+    "aiohttp[speedups]",
+    "jsonrpcclient",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = []
 
 setup(
     author="AIN Dev Team",
-    author_email='dev@ainetwork.ai',
-    python_requires='>=3.7',
+    author_email="dev@ainetwork.ai",
+    python_requires=">3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="AI Network Client Library for Python3",
     install_requires=requirements,
-    license="ISC license",
+    license="MPL license",
     long_description=open("README.md").read(),
     include_package_data=True,
-    keywords='ain',
-    name='ain',
-    packages=find_packages(include=['ain', 'ain.*']),
-    test_suite='tests',
+    keywords=["ain", "ainetwork", "ainblockchain", "API"],
+    name="ain",
+    packages=find_packages(include=["ain", "ain.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/dev-ainetwork/ain',
-    version='0.1.0',
+    url="https://github.com/ainblockchain/ain-py",
+    version="0.1.0",
     zip_safe=False,
 )
