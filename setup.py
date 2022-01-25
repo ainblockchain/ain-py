4ea69795deb7738c7579e16525cd16c2ec8df976
kriii <kriiivv@gmail.com>
2022-01-25 11:51:30 +0900

    Support py3.6

--- setup.py
+++ setup.py
@@ -19,13 +19,14 @@ test_requirements = []
 setup(
     author="AIN Dev Team",
     author_email="dev@ainetwork.ai",
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
