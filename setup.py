bdc93e04010147205ca1d506efc532aa3c249864
kriii <kriiivv@gmail.com>
2022-02-24 19:41:32 +0900

    Version up to 1.0.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ainblockchain/ain-py",
-    version="0.1.4",
+    version="1.0.0",
     zip_safe=False,
 )
