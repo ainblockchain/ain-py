67efe0dd676b57a371dd4f5b35c6f2f42ad9e5da
kriii <kriiivv@gmail.com>
2022-10-01 01:58:18 +0900

    Add sphinx settings

--- docs/conf.py
+++ docs/conf.py
@@ -14,20 +14,22 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'ain-py'
 copyright = '2022, AIN Dev Team'
 author = 'AIN Dev Team'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'readthedocs_ext.readthedocs',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
