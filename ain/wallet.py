fd869a0da1554ec7b6d08bac52381a07ea2afbcc
Dongil Seo <seo@comcom.ai>
2023-10-24 21:24:56 +0900

    Add countDecimals() util function

--- ain/wallet.py
+++ ain/wallet.py
@@ -1,3 +1,5 @@
+import re
+import math
 from typing import TYPE_CHECKING, Any, List, Optional, Union
 from ain.account import Account, Accounts
 from ain.types import (
@@ -57,6 +59,29 @@ class Wallet:
         if checksummed not in self.accounts:
             return ''
         return self.accounts[checksummed].public_key
+
+    @staticmethod
+    def countDecimals(value: float) -> int:
+        """Counts the given number's decimals.
+
+        Args:
+            value(float): The number.
+
+        Returns:
+            int: The decimal count.
+        """
+        valueString = str(value)
+        if math.floor(value) == value :
+            return 0
+        p = re.compile(r'^-{0,1}(\d*\.{0,1}\d*)e-(\d+)$')
+        matches = p.findall(valueString)
+        if len(matches) > 0 and len(matches[0]) == 2:
+            return int(matches[0][1]) + Wallet.countDecimals(float(matches[0][0]))
+        else :
+            parts = valueString.split('.')
+            if len(parts) >= 2 :
+                return len(parts[1])
+            return 0
 
     def create(self, numberOfAccounts: int):
         """Creates `numberOfAccounts` new accounts, and adds them to the wallet.
