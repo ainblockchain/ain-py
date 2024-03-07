1831b48d82d67497a6e51ba8ed27deb4b053cb59
platfowner <seo@comcom.ai>
2024-03-07 10:43:18 +0900

    Upgrade version to v1.1.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ainblockchain/ain-py",
-    version="1.0.3",
+    version="1.1.0",
     zip_safe=False,
 )
